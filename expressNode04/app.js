a3e8446857d8d131cc70548e24069c7b44629056
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-10-05 12:22:59 -0600

    Create app.js

---
+++ expressNode04/app.js
@@ -0,0 +1,31 @@
+// How to server HTML using nodejs.path
+// import path
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Set the router object exported from admin to adminRoutes, which is valid middleware function
+const adminRoutes = require("./routes/admin");
+
+// Set the router object exported from shop to shopRoutes
+const shopRoutes = require("./routes/shop");
+
+// Build Middleware to handle parsing, will assign to obj 'body'
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// insert the adminRoutes obj in middleware to 'activate' those routes,
+// includes '/admin' filter to add common starting url segment
+// prevents us from repeating same url, saving time
+app.use("/admin", adminRoutes);
+
+// insert the shopRoutes obj in middlware to activate those routes
+app.use(shopRoutes);
+
+// set handler for any bad URL, with 404 status()
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
