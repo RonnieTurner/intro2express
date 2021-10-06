2110f1003a376cc6e33eb51b4e0fc2d159d91ead
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-10-06 12:06:06 -0600

    Update path.js

--- expressNode04/util/path.js
+++ expressNode04/util/path.js
@@ -5,3 +5,16 @@ const path = require("path");
 // file that is ran with server is spun up, app.js in this case
 module.exports = path.dirname(require.main.filename);
 // once exported it can be used in any .js file to find the absolute path
+
+/* More on path.dirname(require.main.filename)
+   path.dirname, returns path form of specified directory
+   
+   require.main is the module object representing the entry point
+   script loaded at start and .filename stores the absolute path
+   within the require.main obj 
+   
+   module(
+   filename: 'absolute/path/to/entry.js 
+   )
+   
+   entry.js is described in package.json */
